8c5c51344c1ce68061cb230c86cc5e25855c263e
Irina Kashyntseva Personal <i.kashyntseva@gmail.com>
2018-01-23 23:38:58 +0100

    removed surplus files

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-  gulp.task("sass", function() {
-    gulp.src("scss/styles.scss")
-      .pipe(sass())
-      .pipe(gulp.dest("css"));
-  });
